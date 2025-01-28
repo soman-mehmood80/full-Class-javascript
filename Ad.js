62e7a6f2a1a25c736c38a2984b1786261c3ff797
Urwasohail20 <sohailurwa0@gmail.com>
2025-01-28 15:12:22 +0500

    full javascript

---
+++ Ad.js
@@ -0,0 +1,307 @@
+// var Name="Soman";
+// var F_Name="Mehmood";
+// var Age=19;
+// var Country="Karachi";
+// console.log(Name,F_Name,Age,Country);
+
+// let Student_Name="Ali Hazma";
+// let Student_Age=22;
+// let Student_Class="First Year";
+// let Student_Result="Pass";
+// console.log(Student_Name,Student_Age,Student_Class,Student_Result);
+// document.write(Student_Name,"<br>",Student_Age,"<br>",Student_Class,"<br>",Student_Result);
+
+// let Student_Name="Bilal";
+// let Student_Age=12;
+// let Student_Class=8;
+// let Student_Result="Fail";
+// // console.log(Student_Name,Student_Age,Student_Class,Student_Result);
+// document.write(" Student name Is " + Student_Name,"<br>"," Student Age Is " + Student_Age,"<br>"," Student Class Is " +Student_Class,"<br>"," Student Result Is " +Student_Result);
+
+// let Num1=34;
+// let Num2=10;
+// let total=Num1%Num2;
+// console.log(total);
+
+
+// Class 2 of AD-js
+
+
+// var num1 =10;
+// var num2 =5;
+
+// var a = num1 <= num2;
+// var b = num1 >= num2;
+// var c = num1 == num2;
+// var d = num1 != num2;
+// var e = num1 === num2;
+// var f = num1 > num2;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// document.write(a ,"<br>");
+// document.write(b ,"<br>");
+// document.write(c ,"<br>");
+// document.write(d ,"<br>");
+// document.write(e ,"<br>");
+// document.write(f,"<br>");
+
+// var x=1
+//  if (x===1){
+// document.write("The value is ture");}
+// else{
+//     document.write("The value is fales");
+// }
+ 
+// var a = 4;
+// if(a=!4){
+//     document.write("The value is ture");}
+//     else{
+//         document.write("The value is fales");
+//}
+
+// var age = 18;
+// if(age>= 18 &&  age <=20){
+// document.write("Enter");
+// }
+// else{
+//     document.write("Not Enter");
+// }
+
+// var age = 19;
+// var hight = 4.5;
+// if(age>= 20 || hight <= 4.3){
+//     document.write("Your Are Enter");
+// }
+// else{
+//     document.write("Your Are Not Enter");
+// }
+
+// var age = 20;
+// var hight = 5.0;
+// document.write(! age >=18);
+// var persantage = prompt ("");
+
+// if(persantage >= 90 && persantage <= 100){
+//     document.write("You Got A+");
+// }
+// else if(persantage >= 80 && persantage <=100){
+//     document.write("You Got A");
+// }
+// else if (persantage >= 70 && persantage <=100){
+//     document.write("You Got B");
+// }
+// else if (persantage >= 60 && persantage <= 100){
+//     document.write("You Got C");
+// }
+// else if (persantage >= 50 && persantage <=100){
+//     document.write("You Got D");
+// }
+// else{
+//     document.write("Your Are Failed");
+// }
+
+// var age = prompt() ;
+// var cnic = true;
+
+// if(age >= 18){
+    
+//document.write("You are Eigible For Voting <br>");
+// if(cnic == ture){
+// document.write("You Can give Vote <br>");
+// }
+// }
+// else{
+//     document.write("You Can Not Give Vote");
+// }
+
+// class 3 work
+
+// var day = 6 ;
+// switch (day){
+// case 0 :document.write("Sunday");
+// break;
+// case 1 :document.write("Monday");
+// break;
+// case 2 :document.write("Tuesday");
+// break;
+// case 3 :document.write("Wednesday");
+// break;
+// case 4 :document.write("Thursday");
+// break;
+// case 5 :document.write("Friday");
+// break;
+// case 6 :document.write("Saturday");
+// break;
+// default :document.write("Enter Your Invalid Day");
+// }
+
+// function car_intor (){
+// document.write("My car name is Toyota <br>");
+// document.write("My car color is Black <br>");
+// document.write("My car model is 2012 <br>");
+// document.write("My car price is 500000 <br>");
+// }
+// car_intor();
+
+// With parameter
+
+// function car_intor (name, color, model, price){
+//     document.write("My car is "+ name+ "<br>");
+//     document.write("My car  "+ color +"<br>");
+//     document.write("My car is " + model + "<br>");
+//     document.write("My car is " + price + " <br>");
+//     document.write("<br>")
+//     }
+//     car_intor("Audi",2017,"Black",240000);
+//     car_intor("BMW",2018,"White",340000);
+
+
+
+// Class 4 Work
+
+//     function sum( a , b ){
+// document.write("This is My Sum : " + a + b + "<br>");
+// }
+// sum(12,20);
+
+// function Divi ( a , b ){
+//     document.write("This is My Divi :" + a / b + "<br>");
+//     }
+
+//     Divi(23,4);
+// function Mult ( a , b ){
+//         document.write("This is My Mult :" + a * b + "<br>");
+//         }
+
+//                 Mult(8,2);
+// function Sub ( a , b ){
+//             document.write("This is My Sub :" , a - b , "<br>");
+//             }
+
+//             Sub(52,20);
+// function Per ( a, b ){
+//                 document.write("This is My Per :" , a % b , "<br>");
+//                 }
+//                 Per(1,2);
+
+// function Name(fname,lname){
+
+//     document.write("Hello"+fname+lname);
+
+// }
+
+// Name("Soman","Mehmood");
+
+// let name = prompt("Enter your Name");
+// let age = prompt("Enter your Age");
+// let city = prompt("Enter Your City");
+// alert(name);
+// alert(age);
+// alert(city);
+
+// let num1 =parseInt (prompt("Enter Your First Number"));
+// let num2 =parseInt (prompt("Enter Your Second Number"));
+
+// let sum = num1+num2;
+// alert("Your Answer is "+sum);
+
+// let sub = num1-num2;
+// alert("Your Answer is  "+sub);
+
+// let Mult = num1*num2;
+// alert("Your Answer is "+Mult);
+
+// let Divi = num1/num2;
+// alert("Your Answer is "+Divi);
+
+// let num1 = parseInt(prompt("Enter Your First Number"));
+// let num2 = parseInt (prompt("Enter Your Second Number"));
+// let opertion = prompt("Enter + , - , / , * ");
+
+// switch (opertion){
+//     case "+": var total = num1 + num2;
+//     alert("Your Total Addition is "+ total);
+// break;
+
+// case "-": var total = num1 - num2;
+// alert("Your Total Minus is "+ total);
+// break;
+
+// case "/": var total = num1 / num2;
+// alert("Your Total Division is "+ total);
+// break;
+
+// case "*": var total = num1 * num2;
+// alert("Your Your Total Multiplication is "+ total);
+// break;
+// default : alert("Wrong Input");
+// }
+
+//class 5 work
+
+
+// let Msg = confirm ("Khatam kardu Kiya ?");
+// if (Msg) {
+// alert("Wakiii kar du Khatam");
+// }
+// else {
+//     alert("Khatam nahi karo");}
+//     console.log(Msg);
+
+// let num = prompt("Enter Your Number");
+// for(let i = 1 ; i <= 10; i++){
+// let total = num + "×" + i + " = " + num * i ;
+// document.write(total + "<br>");
+// }
+
+
+// let num = prompt (" Enter your Table Number " );
+// let start = prompt(" Enter Your Starting Number");
+// let End = prompt ( " Enter Your Ending Number");
+                             
+// for(let i = start ; i <= End ; i++){
+//      let Total = num + " X " + i + num*i;
+//      document.write(Total + "<br>");
+// }
+
+
+// for (let i  = 1 ; i <= 10 ; i++){
+//     if (i  % 2 == 0){
+//         console.log("Even Number" + i);
+//     }
+// }
+
+
+// for (let i  = 1 ; i <= 10 ; i++){
+//     if (i  % 2 == 1){
+//         console.log("Odd Number" + i);
+//     }
+// }
+
+
+// for (let i  = 1 ; i <= 10 ; i++){
+// document.write("Saq of " + i + " Is :" + i*i + "<br>");
+// }
+
+
+// let count = 1;
+// while(count <= 10){
+// console.log (count + " Hello World" );
+// count++;
+// }
+
+// Class 8:
+
+// let num = 1;
+// do{
+//     console.log(" Hello World! ");
+//     num++;
+// }
+// while (num <= 10);
+
+
